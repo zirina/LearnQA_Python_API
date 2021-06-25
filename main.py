83368cc51459a2bad8c742994b851ca09ac9b332
IrinaZ <irina.zhelokhovtseva@gmail.com>
2021-06-25 14:32:18 -0700

    Ex4: GET-запрос

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import requests
+
+response =requests.get("https://playground.learnqa.ru/api/get_text")
+print(response.text)
+
+print("==============================")
 print("Hello from Ирина Желоховцева")
+
