2e062c6d617bb08f126134f3b42b7036baecf48c
IrinaZ <irina.zhelokhovtseva@gmail.com>
2021-07-09 14:07:38 -0700

    Ex6: long redirect, steps

--- Ex6_long_redirect.py
+++ Ex6_long_redirect.py
@@ -4,3 +4,5 @@ response = requests.post("https://playground.learnqa.ru/api/long_redirect")
 redirect_num = len(response.history)
 
 print("number of redirect steps is " + str(redirect_num))
+print(response.url)
+
